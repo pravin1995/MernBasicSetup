0eece9c687057129036b41ca1f1a012eb613d5f2
Pravin Salunkhe <pravin28121995@gmail.com>
2018-11-26 16:03:05 +0530

    config file added

---
+++ MERN-boilerplate-master/config/config.example.js
@@ -0,0 +1,6 @@
+// Copy this file as config.js in the same folder, with the proper database connection URI.
+
+module.exports = {
+  db: "mongodb://username:password@localhost:27017/boiler",
+  db_dev: "mongodb://localhost:27017/boiler"
+};
